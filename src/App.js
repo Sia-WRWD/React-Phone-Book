9195cbcb9f6179357eacfb8a38abd5aa18b391d5
Sia-WRWD <82391553+Sia-WRWD@users.noreply.github.com>
2022-08-03 00:38:39 +0800

    First Commit, Completed Landing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/phone.gif";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          <p className="blast">W</p>
+          <p className="blast">e</p>
+          <p className="blast">l</p>
+          <p className="blast">c</p>
+          <p className="blast">o</p>
+          <p className="blast">m</p>
+          <p className="blast">e</p>
+          &nbsp;
+          <p className="blast">t</p>
+          <p className="blast">o</p>
+          &nbsp;
+          <p className="blast">P</p>
+          <p className="blast">-</p>
+          <p className="blast">B</p>
+          <p className="blast">o</p>
+          <p className="blast">o</p>
+          <p className="blast">k</p>
+          <p className="blast">!</p>
+        </h1>
+        <button className="action-btn">View Contacts</button>
+        <button className="action-btn">Add New Contacts</button>
       </header>
+      <footer className="App-footer">
+        <a href="https://heysia.dev">
+          Built and Designed by Sia <br></br> All rights reserved. ©
+        </a>
+      </footer>
     </div>
   );
 }
